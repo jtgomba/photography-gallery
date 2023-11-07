4a5457e7ef3f67376c13cf29bffa3e91b0e54c00
JoelGomba <jgomba@hotmail.co.uk>
2023-11-07 14:07:32 +0900

    feat: scaffold

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,329 @@
 function App() {
-  return <div></div>;
+  return (
+    <div className="loading">
+      <main>
+        <div className="frame">
+          <div className="frame__logo">
+            <h2 className="frame__logo-title">Yamada Taro</h2>
+            <span className="frame__logo-subtitle">フォトグラフィー</span>
+          </div>
+        </div>
+        <div className="content">
+          <div className="content__item">
+            <h2 className="content__item-title">
+              <span className="oh">
+                <span className="oh__inner">Queen of </span>
+              </span>
+              <span className="oh">
+                <span className="oh__inner">the Sea</span>
+              </span>
+            </h2>
+            <div className="content__item-description">
+              <p className="oh">
+                <strong className="oh__inner">Osaka, 1986</strong>
+              </p>
+              <p className="oh">
+                <span className="oh__inner">
+                  Hidesato felt very sorry for the Dragon King on hearing his
+                  story, and readily promised to do what he could to help him.
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="content__item">
+            <h2 className="content__item-title">
+              <span className="oh">
+                <span className="oh__inner">Hinata&apos;s</span>
+              </span>
+              <span className="oh">
+                <span className="oh__inner">Trouble</span>
+              </span>
+            </h2>
+            <div className="content__item-description">
+              <p className="oh">
+                <strong className="oh__inner">Kyoto, 1986</strong>
+              </p>
+              <p className="oh">
+                <span className="oh__inner">
+                  His power was immense, for he was the ruler of all sea
+                  creatures both great and small, and in his keeping were the
+                  Jewels of the Ebb and Flow of the Tide.
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="content__item">
+            <h2 className="content__item-title">
+              <span className="oh">
+                <span className="oh__inner">Dragon&apos;s</span>
+              </span>
+              <span className="oh">
+                <span className="oh__inner">Journey</span>
+              </span>
+            </h2>
+            <div className="content__item-description">
+              <p className="oh">
+                <strong className="oh__inner">Nagoya, 1986</strong>
+              </p>
+              <p className="oh">
+                <span className="oh__inner">
+                  Calling all his fish retainers together, he chose several of
+                  them as ambassadors to go through the sea and seek for a young
+                  Dragon Princess who would be his bride.
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="content__item">
+            <h2 className="content__item-title">
+              <span className="oh">
+                <span className="oh__inner">Prince</span>
+              </span>
+              <span className="oh">
+                <span className="oh__inner">Yamato</span>
+              </span>
+            </h2>
+            <div className="content__item-description">
+              <p className="oh">
+                <strong className="oh__inner">Tokyo, 1986</strong>
+              </p>
+              <p className="oh">
+                <span className="oh__inner">
+                  The King fell in love with her at once, and the wedding
+                  ceremony was celebrated with great splendor.{" "}
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="content__item">
+            <h2 className="content__item-title">
+              <span className="oh">
+                <span className="oh__inner">Kumaso</span>
+              </span>
+              <span className="oh">
+                <span className="oh__inner">&amp; Haru</span>
+              </span>
+            </h2>
+            <div className="content__item-description">
+              <p className="oh">
+                <strong className="oh__inner">Sapporo, 1986</strong>
+              </p>
+              <p className="oh">
+                <span className="oh__inner">
+                  When the hare heard these kind words he felt very grateful to
+                  the man, and encouraged by his gentle manner the hare told him
+                  all that had befallen him.
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="content__item">
+            <h2 className="content__item-title">
+              <span className="oh">
+                <span className="oh__inner">Botan&apos;s</span>
+              </span>
+              <span className="oh">
+                <span className="oh__inner">Dream</span>
+              </span>
+            </h2>
+            <div className="content__item-description">
+              <p className="oh">
+                <strong className="oh__inner">Sendai, 1986</strong>
+              </p>
+              <p className="oh">
+                <span className="oh__inner">
+                  Okuni-nushi-no-Mikoto is worshiped by the people in some parts
+                  of Japan, as a god, and the hare has become famous as “The
+                  White Hare of Inaba”.{" "}
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="content__item">
+            <h2 className="content__item-title">
+              <span className="oh">
+                <span className="oh__inner">The Gate</span>
+              </span>
+              <span className="oh">
+                <span className="oh__inner">of Rashoi</span>
+              </span>
+            </h2>
+            <div className="content__item-description">
+              <p className="oh">
+                <strong className="oh__inner">Kyoto, 1986</strong>
+              </p>
+              <p className="oh">
+                <span className="oh__inner">
+                  Nearly two thousand years ago this sword was kept at the
+                  shrines of Ite, the temples dedicated to the worship of
+                  Amaterasu, the great and beautiful Sun Goddess from whom the
+                  Japanese Emperors are said to be descended.
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="content__item">
+            <h2 className="content__item-title">
+              <span className="oh">
+                <span className="oh__inner">Faithful</span>
+              </span>
+              <span className="oh">
+                <span className="oh__inner">Knights</span>
+              </span>
+            </h2>
+            <div className="content__item-description">
+              <p className="oh">
+                <strong className="oh__inner">Kobe, 1986</strong>
+              </p>
+              <p className="oh">
+                <span className="oh__inner">
+                  At last King Keiko ordered his younger son Prince Yamato to
+                  subdue the brigands and, if possible, to rid the land of their
+                  evil lives.
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="content__item">
+            <h2 className="content__item-title">
+              <span className="oh">
+                <span className="oh__inner">Five Colors</span>
+              </span>
+              <span className="oh">
+                <span className="oh__inner">for Jokwa</span>
+              </span>
+            </h2>
+            <div className="content__item-description">
+              <p className="oh">
+                <strong className="oh__inner">Hiroshima, 1986</strong>
+              </p>
+              <p className="oh">
+                <span className="oh__inner">
+                  There is a story of knightly adventure and daring which
+                  explains why the name of the sword was changed from that of
+                  Murakumo to Kasanagi, which means grass clearing.
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="content__item">
+            <h2 className="content__item-title">
+              <span className="oh">
+                <span className="oh__inner">Shikuyu&apos;s</span>
+              </span>
+              <span className="oh">
+                <span className="oh__inner">Charm</span>
+              </span>
+            </h2>
+            <div className="content__item-description">
+              <p>
+                <strong className="oh">
+                  <span className="oh__inner">Kumamoto, 1986</span>
+                </strong>
+              </p>
+              <p className="oh">
+                <span className="oh__inner">
+                  He at once made ready to start, and great was the stir in the
+                  precincts of the Palace as he and his trusty followers
+                  gathered together and prepared for the expedition, and
+                  polished up their armor and donned it.
+                </span>
+              </p>
+            </div>
+          </div>
+          <button className="content__back unbutton">
+            <svg
+              aria-hidden="true"
+              width="16px"
+              height="16px"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path d="M11.4939 20.5644C11.1821 20.8372 10.7083 20.8056 10.4356 20.4939L3.43557 12.4939C3.18814 12.2111 3.18814 11.7889 3.43557 11.5061L10.4356 3.50613C10.7083 3.1944 11.1822 3.16281 11.4939 3.43557C11.8056 3.70834 11.8372 4.18216 11.5644 4.49388L5.65283 11.25L20 11.25C20.4142 11.25 20.75 11.5858 20.75 12C20.75 12.4142 20.4142 12.75 20 12.75L5.65283 12.75L11.5644 19.5061C11.8372 19.8179 11.8056 20.2917 11.4939 20.5644Z" />
+            </svg>
+            <span className="oh__inner">Back</span>
+          </button>
+          <nav className="content__nav-wrap">
+            <button className="content__nav content__nav--prev unbutton">
+              <svg
+                width="100"
+                height="267"
+                viewBox="0 0 100 267">
+                <path
+                  d="M49.894 2.766v262.979"
+                  strokeLinecap="square"
+                />
+                <path
+                  fill="none"
+                  d="M99.75 76.596C73.902 76.596 52.62 43.07 49.895 0 47.168 43.07 25.886 76.596.036 76.596"
+                />
+              </svg>
+            </button>
+            <button className="content__nav content__nav--next unbutton">
+              <svg
+                width="100"
+                height="267"
+                viewBox="0 0 100 267">
+                <path
+                  d="M49.894 2.766v262.979"
+                  strokeLinecap="square"
+                />
+                <path
+                  fill="none"
+                  d="M99.75 76.596C73.902 76.596 52.62 43.07 49.895 0 47.168 43.07 25.886 76.596.036 76.596"
+                />
+              </svg>
+            </button>
+          </nav>
+        </div>
+        <div className="slides"></div>
+        <div className="stack-wrap">
+          <div className="stack">
+            <div className="stack__item stack__item--empty"></div>
+            <div
+              className="stack__item"
+              style={{ backgroundImage: "url(/img/1.jpg)" }}></div>
+            <div
+              className="stack__item"
+              style={{ backgroundImage: "url(/img/2.jpg)" }}></div>
+            <div
+              className="stack__item"
+              style={{ backgroundImage: "url(/img/3.jpg)" }}></div>
+            <div
+              className="stack__item"
+              style={{ backgroundImage: "url(/img/4.jpg)" }}></div>
+            <div
+              className="stack__item"
+              style={{ backgroundImage: "url(/img/5.jpg)" }}></div>
+            <div
+              className="stack__item"
+              style={{ backgroundImage: "url(/img/6.jpg)" }}></div>
+            <div
+              className="stack__item"
+              style={{ backgroundImage: "url(/img/7.jpg)" }}></div>
+            <div
+              className="stack__item"
+              style={{ backgroundImage: "url(/img/8.jpg)" }}></div>
+            <div
+              className="stack__item"
+              style={{ backgroundImage: "url(/img/9.jpg)" }}></div>
+            <div
+              className="stack__item"
+              style={{ backgroundImage: "url(/img/10.jpg)" }}></div>
+            <div className="stack__item stack__item--empty"></div>
+          </div>
+        </div>
+        <div className="title">
+          <h2 className="title__main oh">
+            <span className="oh__inner">Photography</span>
+          </h2>
+          <span className="title__sub oh">
+            <span className="oh__inner">1986 &mdash; 2022</span>
+          </span>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
